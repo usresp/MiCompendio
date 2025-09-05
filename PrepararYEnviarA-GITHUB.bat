635b84ef5f7eae80917f75151fb47710636d4c16
usresp <ferney.zabala@gmail.com>
2025-09-04 21:16:09 -0500

    Estructura

--- PrepararYEnviarA-GITHUB.bat
+++ PrepararYEnviarA-GITHUB.bat
@@ -35,7 +35,7 @@ if exist ListaArchivosGlobal-cruda.txt (
 echo 🔧 Limpiando rutas absolutas a relativas...
 powershell -Command ^
   "$root = '%dirProyecto%\';" ^
-  "Get-Content 'ListaArchivosGlobal-cruda.txt' | Where-Object { $_ -notmatch '\.git\\' -and $_ -notmatch 'Imagenes\\Usuario\\' -and $_ -notmatch 'Configuracion\\Configuracion\.txt' -and $_ -notmatch 'Configuracion\\Personalizado\.txt' -and $_ -notmatch 'Configuracion\\Usuario\.txt' -and $_ -notmatch 'Listas\\Favoritos\.txt' } | ForEach-Object { '/' + (($_ -replace [regex]::Escape($root), '') -replace '\\', '/') } | Set-Content 'ListaArchivosGlobal.txt'"
+  "Get-Content 'ListaArchivosGlobal-cruda.txt' | Where-Object { $_ -notlike '*\.git\*' -and $_ -notlike '*Imagenes\Usuario\*' -and $_ -notlike '*Configuracion\Configuracion.txt' -and $_ -notlike '*Configuracion\Personalizado.txt' -and $_ -notlike '*Configuracion\Usuario.txt' -and $_ -notlike '*Listas\Favoritos.txt' } | ForEach-Object { '/' + (($_ -replace [regex]::Escape($root), '') -replace '\\', '/') } | Set-Content 'ListaArchivosGlobal.txt'"
 
 if %errorlevel%==0 (
     set estado_limpieza=[OK]
